59bc7ba2de783ae78e305a971c5100b7097a321f
Keerthi Vasan <keerthivasan2197@gmail.com>
2026-09-14 16:17:54 +0530

    Clean repository for hosting

--- src_backup_before_argo/components/online/OnlineVariableSelector.tsx
+++
@@ -1,8 +0,0 @@
-import { MenuItem, Select } from '@mui/material';
-import type { OnlineVariable } from '../../api/onlineApi';
-
-export function OnlineVariableSelector({ variables, value, onChange, disabled }: { variables: OnlineVariable[]; value: string; onChange: (value: string) => void; disabled?: boolean }) {
-  return <Select fullWidth size="small" value={value} disabled={disabled} onChange={(event) => onChange(event.target.value)}>
-    {variables.map((variable) => <MenuItem key={variable.id} value={variable.id}>{variable.name} ({variable.units})</MenuItem>)}
-  </Select>;
-}
--- src_backup_before_argo/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_MAPTILER_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
